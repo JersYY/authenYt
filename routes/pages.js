177b31ee6007736c0fd4ad21607f5c1ad817ce90
Joshua <joshuahutasoit809@gmail.com>
2025-05-20 17:34:52 +0700

    Update pages.js

--- routes/pages.js
+++ routes/pages.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const router = express.Router();
 const authMiddleware = require('../middleware/auth');
+const authController = require('../controllers/auth');
+
 
 // Apply middleware to retrieve user for all routes
 router.use(authMiddleware.getUser);
